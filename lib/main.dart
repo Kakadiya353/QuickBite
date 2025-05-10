58c313cc03335bebdc1f4053d88b92fa207c9456
Kakadiya Chiranj <ckakadiya353@rku.ac.in>
2025-05-10 18:49:49 +0530

    Update main.dart
    ->Notification functionality enhance user experience by getting updates on there apps, placing orders, adding money to wallet

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:awesome_notifications/awesome_notifications.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
@@ -7,13 +8,29 @@ import 'package:quickbite/controller/login_controller.dart';
 import 'package:quickbite/controller/themeprovider.dart';
 import 'package:quickbite/firebase_optiion.dart';
 import 'package:quickbite/pages/bottomnav.dart';
-import 'package:quickbite/pages/onBoard.dart';
 
 Future<void> main() async {
   await GetStorage.init();
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(options: firebaseOptions);
   Get.put(LoginController());
+  AwesomeNotifications().initialize(
+    'resource://drawable/app_icon_notification_white',
+    [
+      NotificationChannel(
+        channelKey: "Basic_channel_v2",
+        channelName: "Basic_Notification",
+        channelDescription: "Notification for basic check",
+        defaultColor: Color(0xFF9D50DD),
+        ledColor: Colors.white,
+        importance: NotificationImportance.Max,
+        // High priority
+        soundSource: 'resource://raw/custom_notification_sound', // Custom sound
+      ),
+    ],
+    debug: true,
+  );
+
   runApp(
     ChangeNotifierProvider(
       create: (context) => ThemeProvider(),
@@ -22,34 +39,52 @@ Future<void> main() async {
   );
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  void initState() {
+    super.initState();
+    AwesomeNotifications().isNotificationAllowed().then((isAllowed) {
+      if (!isAllowed) {
+        AwesomeNotifications().requestPermissionToSendNotifications();
+      }
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     final themeProvider = Provider.of<ThemeProvider>(context);
-    return GetMaterialApp(
-        debugShowCheckedModeBanner: false,
-        title: 'Flutter Demo',
-        themeMode: themeProvider.themeMode,
-        theme: ThemeData(
-          brightness: Brightness.light,
-          primarySwatch: Colors.deepPurple,
-          scaffoldBackgroundColor: Colors.white,
-          textTheme: const TextTheme(
-            bodyLarge: TextStyle(color: Colors.black),
-            bodyMedium: TextStyle(color: Colors.black),
+    return SafeArea(
+      child: GetMaterialApp(
+          debugShowCheckedModeBanner: false,
+          title: 'Flutter Demo',
+          themeMode: themeProvider.themeMode,
+          theme: ThemeData(
+            brightness: Brightness.light,
+            primarySwatch: Colors.deepPurple,
+            scaffoldBackgroundColor: Colors.white,
+            textTheme: const TextTheme(
+              bodyLarge: TextStyle(color: Colors.black),
+              bodyMedium: TextStyle(color: Colors.black),
+            ),
           ),
-        ),
-        darkTheme: ThemeData(
-          brightness: Brightness.dark,
-          primarySwatch: Colors.deepPurple,
-          scaffoldBackgroundColor: Colors.black,
-          textTheme: const TextTheme(
-            bodyLarge: TextStyle(color: Colors.white),
-            bodyMedium: TextStyle(color: Colors.white),
+          darkTheme: ThemeData(
+            brightness: Brightness.dark,
+            primarySwatch: Colors.deepPurple,
+            scaffoldBackgroundColor: Colors.black,
+            textTheme: const TextTheme(
+              bodyLarge: TextStyle(color: Colors.white),
+              bodyMedium: TextStyle(color: Colors.white),
+            ),
           ),
-        ),
-        home: OnBoard());
+          home: BottomNav()),
+    );
   }
 }
+
+//₹
